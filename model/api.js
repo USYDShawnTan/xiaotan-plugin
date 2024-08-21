00defb37b5640cd801d195c9b30969e21d13a0ca
USYDShawnTan <a1273606617@163.com>
2024-08-21 13:55:15 +0800

    🐱

--- model/api.js
+++ model/api.js
@@ -68,6 +68,57 @@ class Api {
       message: buildFortuneMessage(fortuneData),
     };
   }
-}
+  async longtu(e) {
+    const base_url = "https://git.acwing.com/XT/long/-/raw/main/";
+    const batches = {
+      1: [1, 190, ".gif"],
+      2: [191, 522, ".jpg"],
+      3: [523, 854, ".jpg"],
+      4: [855, 1185, ".jpg"],
+      5: [1186, 1516, ".jpg"],
+    };
 
+    const batch_choice =
+      Object.keys(batches)[
+        Math.floor(Math.random() * Object.keys(batches).length)
+      ];
+    const [start, end, extension] = batches[batch_choice];
+    const image_number = Math.floor(Math.random() * (end - start + 1)) + start;
+    const image_url = `${base_url}${batch_choice}/${image_number}${extension}`;
+
+    try {
+      await e.reply(segment.image(image_url), false);
+      return true;
+    } catch (error) {
+      console.error("Error sending image:", error);
+      await e.reply("出错啦~稍后再试噢");
+      return false;
+    }
+  }
+  async kuntu(e) {
+    const base_url = "https://git.acwing.com/XT/ikun/-/raw/main/";
+    const batches = {
+      1: [1, 100, ".jpg"],
+      2: [101, 199, ".jpg"],
+      3: [200, 298, ".jpg"],
+      4: [299, 397, ".jpg"],
+      5: [398, 496, ".jpg"],
+    };
+    const batch_choice =
+      Object.keys(batches)[
+        Math.floor(Math.random() * Object.keys(batches).length)
+      ];
+    const [start, end, extension] = batches[batch_choice];
+    const image_number = Math.floor(Math.random() * (end - start + 1)) + start;
+    const image_url = `${base_url}${batch_choice}/${image_number}${extension}`;
+    try {
+      await e.reply(segment.image(image_url), false);
+      return true;
+    } catch (error) {
+      console.error("Error sending image:", error);
+      await e.reply("出错啦~稍后再试噢");
+      return false;
+    }
+  }
+}
 export default new Api();
