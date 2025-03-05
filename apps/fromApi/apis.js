2e037de922fa31ab0d439cdd3295eb800c3aee8d
USYDShawnTan <xiaotanyyds@foxmail.com>
2025-03-05 11:32:19 +0800

    🐱

--- apps/fromApi/apis.js
+++ apps/fromApi/apis.js
@@ -168,7 +168,7 @@ export class api extends plugin {
       e.reply("出错啦~稍后再试噢");
     }
   }
-  //已经用不了
+  //已经用不了😭
   async sh(e) {
     // API的URL
     const url = "https://api.vvhan.com/api/text/sexy";
